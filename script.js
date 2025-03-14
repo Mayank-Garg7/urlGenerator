a4526c64fbf6d1d35fbecabcf574434349391252
Mayank Garg <113042462+Mayank-Garg7@users.noreply.github.com>
2025-03-14 21:34:15 +0530

    adding the frontend scripting for client side features

--- script.js
+++ script.js
@@ -1,1 +1,25 @@
-let display;
+erInput = document.getElementById("input");
+let urlOutput = document.getElementById("output");
+let toggleButton = document.getElementById("toggleButton");
+let searchGenerated = false;
+
+function generateUrl() {
+    if (userInput.value == "") {
+        alert("Search something, you can't leave it empty...")
+    } else {
+        if (!searchGenerated) {
+            let baseUrl = "https://www.google.com/search?q="; //This is the base url of google 
+            let safeQuery = encodeURIComponent(userInput.value);
+            let finalUrl = baseUrl + safeQuery;
+            urlOutput.value = finalUrl;
+            toggleButton.innerText = "Copy URL";
+            searchGenerated = true;
+        } else {
+            navigator.clipboard.writeText(urlOutput.value).then(() => {
+                alert("URL copied to clipboard!");
+            });
+            toggleButton.innerText = "Search";
+            searchGenerated = false;
+        }
+    }
+}
